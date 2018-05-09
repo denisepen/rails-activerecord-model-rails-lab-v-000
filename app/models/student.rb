53da025806054ca7b563ba3fbd4aa421b48cfdc9
Denise Pendleton <dpendl1@hotmail.com>
2018-05-09 17:48:30 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
 
   def to_s
     @student = Student.create
-    binding.pry
+    # binding.pry
      puts "{@student.first_name}" + "{@student.last_name}"
   end
 end
