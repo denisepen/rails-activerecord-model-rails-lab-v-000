ccb15663ec03437f401f9f036730170fdcce34de
Denise Pendleton <dpendl1@hotmail.com>
2018-05-09 17:37:33 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,6 @@ class Student < ActiveRecord::Base
 
   def to_s
     @student = Student.create
-     @student.first_name + @student.last_name
+     @student.first_name "+" @student.last_name
   end
 end
